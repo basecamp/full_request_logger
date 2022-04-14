14a235761d38bc1997bd4e25231e0b513370d500
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 19:08:19 +0800

    Delete lib directory

--- lib/full_request_logger/job.rb
+++
@@ -1,9 +0,0 @@
-module FullRequestLogger::Job
-  extend ActiveSupport::Concern
-
-  included do
-    alias_method :request_id, :job_id
-
-    after_perform { |job| FullRequestLogger::Processor.new(job).process }
-  end
-end
