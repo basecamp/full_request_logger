b8b2ac6a337dd8bea925e2afc8b9c8567bcc5001
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 19:08:03 +0800

    Delete config directory

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  scope "rails/conductor/full_request_logger/", module: "rails/conductor/full_request_logger" do
-    resources :request_logs, only: %i[ index create show ], as: :rails_conductor_request_logs
-  end
-end
